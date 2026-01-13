12236f8c9b064405e9029d906479b220ce30b1eb
Maithil Korat <maithilkorat2006@gmail.com>
2026-01-13 13:20:07 +0530

    Update database connection to use dotenv
    Refactor database connection to use environment variables.

--- db.py
+++ db.py
@@ -1,18 +1,13 @@
+import os
 import mysql.connector
-from mysql.connector import pooling
+from dotenv import load_dotenv
 
-dbconfig = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "cattle_milk"
-}
-
-cnxpool = pooling.MySQLConnectionPool(
-    pool_name="mypool",
-    pool_size=5,
-    **dbconfig
-)
+load_dotenv()  # only for local
 
 def get_db():
-    return cnxpool.get_connection()
+    return mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
